d78dd3368b9dca9517b62c006c5288bd0bd6ed1d
Pablo León <p.leon@dipualba.es>
2023-11-17 09:23:54 +0100

    Añado proyecto de tests

---
+++ DipuAlba.Artilugios.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using NUnit.Framework;
+
+namespace DipuAlba.Artilugios.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestExcepciones()
+        {
+            var ex1 = new Exception("Excepción 1");
+            var ex2 = new Exception("Excepción 2", ex1);
+            var ex3 = new Exception("Excepción 3", ex2);
+
+            Assert.Pass(ex3.GetInnerExceptionsConcatMessage());
+        }
+    }
+}
