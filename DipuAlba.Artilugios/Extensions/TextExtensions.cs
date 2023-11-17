706a08e50f275274b10bf5ff212ebd5c033a4a91
Pablo León <p.leon@dipualba.es>
2023-11-17 10:29:02 +0100

    Añade la clase TextExtensions
    Con un método RemoveDiacritics para eliminar los diacríticos de una cadena de texto.

---
+++ DipuAlba.Artilugios/Extensions/TextExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace DipuAlba.Artilugios.Extensions
+{
+    public static class TextExtensions
+    {
+        public static string RemoveDiacritics(this string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder
+                .ToString()
+                .Normalize(NormalizationForm.FormC);
+        }
+    }
+}
