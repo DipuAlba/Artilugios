d8d3f7b481607f3c9bf50582908d07c494c4c792
Pablo León <p.leon@dipualba.es>
2023-11-20 08:53:10 +0100

    Actualiza generarnuget.cmd y SeDipuAlba.Artilugios.csproj
    - Actualiza generarnuget.cmd para incluir la propiedad "mycommit" en el comando nuget pack.
    - Actualiza SeDipuAlba.Artilugios.csproj con información adicional del paquete, como autores, URL del proyecto y URL del repositorio.
    - Añade un elemento ItemGroup en SeDipuAlba.Artilugios.csproj para incluir el archivo readme.md en el paquete.
    - Actualiza SeDipuAlba.Artilugios.nuspec con la URL del repositorio y la propiedad "mycommit".

--- SeDipuAlba.Artilugios/generarnuget.cmd
+++ SeDipuAlba.Artilugios/generarnuget.cmd
@@ -1,5 +1,5 @@
 @echo off
 for /f "delims=" %%x in ('dir /od /b *.nupkg') do move %%x oldnupkg\
-nuget pack -Prop Configuration=Release
+nuget pack -Properties "Configuration=Release;mycommit=%build.vcs.number.1%"
 for /f "delims=" %%x in ('dir /od /b *.nupkg') do set recent=%%x
 echo %recent%
