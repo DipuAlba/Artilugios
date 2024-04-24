a9864a811d8be11f06ca68fa661d77ad22dfc332
Pablo León <p.leon@dipualba.es>
2024-04-24 11:56:24 +0200

    Added TimeConversion utility class
    This commit introduces a new utility class, TimeConversion. This class provides methods for converting Unix and Java timestamps to DateTime objects. The conversion is based on the number of seconds (Unix) or milliseconds (Java) that have elapsed since January 1, 1970 (UTC). The resulting DateTime object is adjusted to local time.

---
+++ SeDipuAlba.Artilugios/TimeConversion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SeDipuAlba.Artilugios
+{
+    /// <summary>
+    /// Provides utility methods for converting time formats to DateTime.
+    /// Based on ScottCher code: https://stackoverflow.com/a/250400/2126607
+    /// </summary>
+    public class TimeConversion
+    {
+        /// <summary>
+        /// Converts a Unix timestamp to a DateTime object.
+        /// </summary>
+        /// <param name="unixTimeStamp">The Unix timestamp to convert, represented as a double. 
+        /// This value represents the number of seconds that have elapsed since January 1, 1970 (UTC).</param>
+        /// <returns>The converted DateTime object adjusted to local time.</returns>
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Define the starting point as January 1, 1970 (the Unix epoch) in UTC.
+            var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            // Add the number of seconds given by unixTimeStamp to the epoch.
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            // Convert to local time from UTC.
+            return dateTime;
+        }
+
+        /// <summary>
+        /// Converts a Java timestamp to a DateTime object.
+        /// </summary>
+        /// <param name="javaTimeStamp">The Java timestamp to convert, represented as a double.
+        /// This value represents the number of milliseconds that have elapsed since January 1, 1970 (UTC).</param>
+        /// <returns>The converted DateTime object adjusted to local time.</returns>
+        public static DateTime JavaTimeStampToDateTime(double javaTimeStamp)
+        {
+            // Define the starting point as January 1, 1970 (the Unix epoch) in UTC.
+            var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            // Add the number of milliseconds given by javaTimeStamp to the epoch.
+            dateTime = dateTime.AddMilliseconds(javaTimeStamp).ToLocalTime();
+            // Convert to local time from UTC.
+            return dateTime;
+        }
+    }
+}
