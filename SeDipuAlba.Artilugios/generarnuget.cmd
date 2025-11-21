ad2e47a8d96d2702b2353752b606f07f0d734ce3
Pablo León <p.leon@dipualba.es>
2025-11-21 12:40:48 +0100

    Script para empaquetar proyecto con dotnet pack

--- SeDipuAlba.Artilugios/generarnuget.cmd
+++ SeDipuAlba.Artilugios/generarnuget.cmd
@@ -1,5 +1,12 @@
 @echo off
+
+REM Borra el nupkg anterior moviéndolo a oldnupkg
+if not exist oldnupkg mkdir oldnupkg
 for /f "delims=" %%x in ('dir /od /b *.nupkg') do move %%x oldnupkg\
-nuget pack -Properties "Configuration=Release;mycommit=%build.vcs.number.1%"
+
+REM Empaqueta el proyecto usando dotnet pack (esto funciona para proyectos SDK-style)
+dotnet pack SeDipuAlba.Artilugios.csproj --configuration Release --output .
+
+REM Busca el nupkg más reciente para asignarlo a la variable
 for /f "delims=" %%x in ('dir /od /b *.nupkg') do set recent=%%x
 echo %recent%
